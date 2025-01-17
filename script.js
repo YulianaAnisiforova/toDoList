3561993df3a3fe8a377bca213ace457f8a6a4d04
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-01-17 15:06:05 +0300

    trying to add color theme

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let colorSelection = document/querySelector('.colorSelection')
+let colorSelection = document.querySelector('.colorSelection')
 let list = document.querySelector('.list')
 let addElement = document.querySelector('.addElement')
 let addButton = document.querySelector('.addButton')
 
+let currentColor = 'light theme'
 colorSelection.onclick = () => {
-    let val = colorSelection[colorSelection.selectedIndex].value
-    if (val == 'light theme') {
+    let val = colorSelection.options[colorSelection.selectedIndex].value
+    if (val == 'light theme' && currentColor == 'dark theme') {
+        currentColor = 'light theme'
         colorSelection.style = 'background-color: rgb(246, 234, 218)'
-    } else {
+    } else if (val == 'dark theme' && currentColor == 'light theme') {
+        currentColor = 'dark theme'
         colorSelection.style = 'background-color: rgb(67, 59, 48)'
+        // colorSelection.style = 'color: rgb(246, 234, 218)'
     }
 }
+
