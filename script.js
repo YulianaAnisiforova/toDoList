9166fae3e15723a319ef7b7d39bf68f65bfe5e58
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-01-17 16:36:44 +0300

    added theme switch

--- script.js
+++ script.js
@@ -2,17 +2,39 @@ let colorSelection = document.querySelector('.colorSelection')
 let list = document.querySelector('.list')
 let addElement = document.querySelector('.addElement')
 let addButton = document.querySelector('.addButton')
+let mainBack = document.body
 
-let currentColor = 'light theme'
-colorSelection.onclick = () => {
+colorSelection.addEventListener("change", (event) => {
     let val = colorSelection.options[colorSelection.selectedIndex].value
-    if (val == 'light theme' && currentColor == 'dark theme') {
-        currentColor = 'light theme'
-        colorSelection.style = 'background-color: rgb(246, 234, 218)'
-    } else if (val == 'dark theme' && currentColor == 'light theme') {
-        currentColor = 'dark theme'
-        colorSelection.style = 'background-color: rgb(67, 59, 48)'
-        // colorSelection.style = 'color: rgb(246, 234, 218)'
+    if (val == 'light theme') {
+        colorSelection.style.background = 'rgb(246, 234, 218)'
+        colorSelection.style.color = 'rgb(67, 59, 48)'
+        colorSelection.style.border = '1px solid rgb(67, 59, 48)'
+
+        mainBack.style.background = 'rgb(246, 234, 218)'
+
+        addButton.style.background = 'rgb(67, 59, 48)'
+        addButton.style.color = 'rgb(253, 249, 244)'
+
+        list.style.background = 'rgb(253, 249, 244)'
+
+        addElement.style.background = 'rgb(253, 249, 244)'
+        addElement.style.border = '1px solid rgb(67, 59, 48)'
+
+    } else if (val == 'dark theme') {
+        colorSelection.style.background = 'rgb(67, 59, 48)'
+        colorSelection.style.color = 'rgb(246, 234, 218)'
+        colorSelection.style.border = '1px solid rgb(246, 234, 218)'
+
+        mainBack.style.background = 'rgb(67, 59, 48)'
+
+        addButton.style.background = 'rgb(246, 234, 218)'
+        addButton.style.color = 'rgb(67, 59, 48)'
+
+        list.style.background = 'rgb(208, 186, 158)'
+
+        addElement.style.background = 'rgb(241, 218, 187'
+        addElement.style.border = '1px solid rgb(246, 234, 218)'
     }
-}
+});
 
